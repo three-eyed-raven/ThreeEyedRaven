d74a57976c75505bc4d0b53b32e9f4e43df1792d
Fiona Thompson <FionaBronwen@gmail.com>
2017-03-28 00:59:04 -0400

    Character view controller (#6)
    * Import custom assets, create resuable character cell
    
    * Search bar functionality
    
    * Update navbar and tab bar styling
    
    * Create extension for search bar functions

---
+++ Three Eyed Raven/CharacterCell.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterCell.swift
+//  Three Eyed Raven
+//
+//  Created by Fiona Thompson on 3/27/17.
+//
+//
+
+import UIKit
+
+class CharacterCell: UITableViewCell {
+
+    @IBOutlet weak var characterNameLabel: UILabel!
+    @IBOutlet weak var characterHouseLabel: UILabel!
+    @IBOutlet weak var characterDescriptionLabel: UILabel!
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Three Eyed Raven/CharactersViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CharactersViewController.swift
+//  Three Eyed Raven
+//
+//  Created by Fiona Thompson on 3/27/17.
+//
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let searchBar = UISearchBar()
+    let tabBar = UITabBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        self.navigationItem.titleView = searchBar
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell") as! CharacterCell
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CharactersViewController: UISearchBarDelegate {
+    
+    public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.tintColor = UIColor.black
+        searchBar.showsCancelButton = true
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+}
