269ea735c0a41bd975fff32de80116e09d22f7d7
fionabronwen <fionabronwen@gmail.com>
2017-04-04 17:18:42 -0400

    Create house detail view controller class, import Stark sigil asset, layout changes.

--- Three Eyed Raven/HouseCell.swift
+++ Three Eyed Raven/HouseCell.swift
@@ -12,4 +12,11 @@ class HouseCell: UICollectionViewCell {
     
     @IBOutlet weak var houseImageView: UIImageView!
     @IBOutlet weak var houseNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        houseImageView.layer.cornerRadius = 3
+        houseImageView.clipsToBounds = true
+    }
+
 }
