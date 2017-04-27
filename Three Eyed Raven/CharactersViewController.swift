62f038450978c8c499b407472d1a0fa73cef260c
William Huang <huang554@purdue.edu>
2017-04-27 15:24:03 -0400

    Character search (#30)
    * Add search bar to nav bar
    
    * Implement search table view
    
    * Finish character search
    
    * Remove typo
    
    * Add gender and played by

--- Three Eyed Raven/CharactersViewController.swift
+++ Three Eyed Raven/CharactersViewController.swift
@@ -14,10 +14,15 @@ import RealmSwift
 class CharactersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchTableView: UITableView!
     var characterIndex = 0
     var storedCharacters: [RealmCharacter] = []
+    var filteredCharacters: [RealmCharacter] = []
+    var characterForSearch: Character?
     var characters: [Character] = []
     let searchBar = UISearchBar()
+    var searchButton = UIBarButtonItem()
+    var titleView = UIView()
     let tabBar = UITabBar()
     var isMoreDataLoading = false
     var loadingMoreView: InfiniteScrollActivityView?
@@ -27,14 +32,15 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
         let realm = try! Realm()
         tableView.delegate = self
         tableView.dataSource = self
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 200
+        searchTableView.rowHeight = UITableViewAutomaticDimension
+        searchTableView.estimatedRowHeight = 200
         
         searchBar.delegate = self
-        
-        //tabBar.tintColor = UIColor.darkGray
-        //self.navigationItem.titleView = searchBar
         setNavigationBarButtons()
         
         // Set up Infinite Scroll loading indicator
@@ -50,6 +56,7 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
         GoTClient.downloadCharacters(success: { 
             let characters = realm.objects(RealmCharacter.self).sorted(byKeyPath: "name")
             self.storedCharacters = Array(characters)
+            self.filteredCharacters = self.storedCharacters
             MBProgressHUD.hide(for: self.tableView, animated: true)
             self.fetchCharacters()
         }) { 
@@ -63,11 +70,11 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
         let logoView = UIImageView(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
         logoView.image = logoImage
         logoView.contentMode = .scaleAspectFit
-        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
+        self.titleView = UIView(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
         logoView.frame = titleView.bounds
         titleView.addSubview(logoView)
         
-        let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: nil)
+        self.searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(searchIconPressed(sender:)))
         
         self.navigationItem.rightBarButtonItem = searchButton
         self.navigationItem.titleView = titleView
@@ -100,10 +107,21 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == self.searchTableView {
+            return filteredCharacters.count
+        }
         return characters.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == self.searchTableView {
+            let cell = searchTableView.dequeueReusableCell(withIdentifier: "CharacterSearchCell") as! CharacterSearchCell
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(searchCellPressed(sender:)))
+            cell.addGestureRecognizer(tapGestureRecognizer)
+            let character = filteredCharacters[indexPath.row]
+            cell.characterNameLabel.text = character.name
+            return cell
+        }
         let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell") as! CharacterCell
         cell.characterImageView.image = UIImage(named: "stock-character-image")
         let character = characters[indexPath.row]
@@ -117,6 +135,43 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
         return cell
     }
     
+    func searchCellPressed(sender: UITapGestureRecognizer) {
+        MBProgressHUD.showAdded(to: self.searchTableView, animated: true)
+        let group = DispatchGroup()
+        let cell = sender.view as! CharacterSearchCell
+        if let indexPath = self.searchTableView.indexPath(for: cell) {
+            let character = filteredCharacters[indexPath.row]
+            group.enter()
+            GoTClient.getCharacter(fromUrlString: character.urlString, success: { (character: Character) in
+                self.characterForSearch = character
+                group.enter()
+                GoTClient.setHouse(for: character, success: {
+                    print("1")
+                    group.leave()
+                }, failure: {
+                    print("2")
+                    group.leave()
+                })
+                group.enter()
+                GoTClient.getCharacterPhoto(characters: [character], success: {
+                    print("3")
+                    group.leave()
+                }, failure: {
+                    print("4")
+                    group.leave()
+                })
+                group.leave()
+            }, failure: { 
+                print("Character search failed")
+            })
+        }
+        group.notify(queue: .main) {
+            
+            MBProgressHUD.hide(for: self.searchTableView, animated: true)
+            self.performSegue(withIdentifier: "CharacterDetailSegue", sender: cell)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -128,6 +183,11 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let characterDetailVC = segue.destination as! CharacterDetailViewController
+        self.searchBar.resignFirstResponder()
+
+        if ((sender as? CharacterSearchCell) != nil) {
+            characterDetailVC.character = self.characterForSearch
+        }
         if let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell) {
             let character = characters[indexPath.row]
             characterDetailVC.character = character
@@ -137,6 +197,15 @@ class CharactersViewController: UIViewController, UITableViewDelegate, UITableVi
 
 extension CharactersViewController: UISearchBarDelegate {
     
+    func searchIconPressed(sender: UIBarButtonItem) {
+        self.navigationItem.rightBarButtonItem = nil
+        self.searchBar.becomeFirstResponder()
+        self.navigationItem.titleView = self.searchBar
+        self.tableView.isHidden = true
+        self.searchTableView.isHidden = false
+        self.searchTableView.reloadData()
+    }
+    
     public func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.tintColor = UIColor.gold
         searchBar.showsCancelButton = true
@@ -144,11 +213,24 @@ extension CharactersViewController: UISearchBarDelegate {
 
     }
     
+    public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("text changing")
+        
+        filteredCharacters = searchText.isEmpty ? storedCharacters : storedCharacters.filter({(character: RealmCharacter) -> Bool in
+            let name = character.name
+            return name.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        self.searchTableView.reloadData()
+    }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.showsCancelButton = false
         searchBar.text = ""
         searchBar.resignFirstResponder()
+        self.navigationItem.titleView = self.titleView
+        self.navigationItem.rightBarButtonItem = self.searchButton
+        self.tableView.isHidden = false
+        self.searchTableView.isHidden = true
     }
 }
 
